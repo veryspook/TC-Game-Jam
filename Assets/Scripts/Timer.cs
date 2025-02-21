c73532ce1643b3b41895504cd54c22b70ab2557a
Joshua Almario <joshua122204@gmail.com>
2025-02-21 15:24:07 -0800

    added timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,21 +7,15 @@ public class Timer : MonoBehaviour
     private string timer;
     private float time;
     
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        if (DialogueManager.ended) {
+            time += Time.deltaTime;
+        }
         int minutes = (int)(time / 60); 
         int seconds = (int)(time % 60); 
         float milliseconds = (time * 1000) % 1000; // Extract milliseconds
-        timer = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, (int)milliseconds);
+        timer = string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, (int)milliseconds);
         timerText.text = timer;
     }
 }
